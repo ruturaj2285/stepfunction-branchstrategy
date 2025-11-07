7df9a871fea005ce85a3b00c577b8e010f6eb020
Ruturaj <ruturajgidde6460@gmail.com>
2025-11-07 10:19:59 +0530

    test: update data collector lambda

--- functions/data-collector/handler.py
+++ functions/data-collector/handler.py
@@ -1,6 +1,7 @@
 import json
 
 def lambda_handler(event, context):
+    print("New version of data collector")
     return {
         "statusCode": 200,
         "body": json.dumps({"ok": True, "service": "data-collector"})
